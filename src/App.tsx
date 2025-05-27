755fcc037fbf2cf6a330d00f4a16805a36399ddc
Abu Horaira <syedmdabuhoraira@gmail.com>
2025-05-27 20:34:30 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="flex w-full items-center mx-auto justify-center h-screen gap-4">
+        <button className="bg-green-600 border-white font-semibold rounded-2xl text-white text-lg shadow-2xl px-4 py-2"> Increment</button>
+        <h1>0</h1>
+        <button className="bg-red-600 font-semibold rounded-2xl text-white text-lg shadow-2xl px-4 py-2">Decrement</button>
+      </div>
+
+    </>
+  )
+}
+
+export default App
